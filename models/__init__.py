a8dd38edf2289a9565d7551b652b85ea1eb08d68
GongCheng <gongcheng_nankai@163.com>
2020-07-11 14:45:12 +0800

    add models

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
 from .VGG_like import VGG7_N
+from .lenet5_architecture import LeNet5
+from .vgglike_architecture import VGG_like
+from .alexnet_architecture import AlexNet
+from .resnet18_architecture import ResNet18
+from .mobilenetv1_architecture import MobileNetV1
+from .mobilenetv2_architecture import MobileNetV2
